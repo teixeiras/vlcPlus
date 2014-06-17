076e0b3bc637fd9f0bdc38b3962ce80029ae4619
Filipe Teixeira <teixeiras@gmail.com>
2014-06-17 15:33:33 +0100

    Add support to opensubtitele API

---
+++ Sources/VLCSubtitleManagerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VLCSubtitleManagerTableViewController.h
+//  VLC for iOS
+//
+//  Created by Filipe Teixeira on 17/06/14.
+//  Copyright (c) 2014 VideoLAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VLCSubtitleManagerTableViewController : UITableViewController
+
+@end
